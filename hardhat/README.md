e0b745bef44d692c85473aed05241b3911f0d0c7
Jimmy Chu <jimmychu0807@gmail.com>
2023-12-07 11:11:03 +0800

    Update with dotenv and rm hardhat/deployments info (#14)
    * Updated with dotenv
    
    * rm hardhat/deployments dir

--- hardhat/README.md
+++ hardhat/README.md
@@ -7,3 +7,10 @@ There are:
 
 - [Flipper Solidity smart contract](./contracts/Flipper.sol)
 - [Proof of Existence Solidity smart contract](./contracts/ProofOfExistence.sol)
+
+## Helpful Commands
+
+```bash
+# Deploy contracts
+pn hardhat deploy --reset --tags <your-tag> --network cess-testnet
+```
--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ethers";
 import "hardhat-deploy";
+import 'dotenv/config';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
@@ -29,6 +30,11 @@ const config: HardhatUserConfig = {
       chainId: 11330,
       // private key of `//Alice` from Substrate
       accounts: ["0xe5be9a5092b81bca64be81d212e7f2f9eba183bb7a90954f7b76361f6edb5c0a"],
+    },
+    "cess-testnet": {
+      url: "https://testnet-rpc0.cess.cloud/ws/",
+      chainId: 11330,
+      accounts: [process.env.DEPLOYMENT_SK || ""],
     }
   }
 };
